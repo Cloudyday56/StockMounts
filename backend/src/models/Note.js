f2704d369384d483b8c0bcc8bffd1ab35a17fe78
Cloudyday56 <Rchen78900@gmail.com>
2025-06-19 18:05:23 -0400

    DB setup #2

--- backend/src/models/Note.js
+++ backend/src/models/Note.js
@@ -1,1 +1,27 @@
 
+import mongoose from "mongoose";
+
+// 1. create schema
+// 2. create model based on the schema
+
+const noteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+//   createdAt: {
+//     type: Date,
+//     default: Date.now, // Automatically sets the current date and time
+//   },
+}, {
+  timestamps: true, // Automatically adds createdAt and updatedAt fields
+});
+
+
+const Note = mongoose.model("Note", noteSchema);
+
+export default Note;
