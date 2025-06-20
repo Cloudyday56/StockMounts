585f13330b1faba50cb057620862b130731b46db
Cloudyday56 <Rchen78900@gmail.com>
2025-06-20 15:15:08 -0400

    Frontend setup #2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router';
 
+import {Toaster} from 'react-hot-toast';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Toaster />
     </BrowserRouter>
   </StrictMode>,
 )
