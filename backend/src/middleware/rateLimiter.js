5ba2b834e0b8e4ef596efec74728fdf6b193fbc1
Cloudyday56 <Rchen78900@gmail.com>
2025-07-24 12:47:41 -0400

    Auth system - more middlewares

--- backend/src/middleware/rateLimiter.js
+++ backend/src/middleware/rateLimiter.js
@@ -2,9 +2,9 @@ import ratelimit from "../config/upstash.js";
 
 
 const rateLimiter = async (req, res, next) => {
-
+  const userId = req.user ? req.user.id : "anonymous"; // Use user ID if authenticated, otherwise use a generic identifier
   try {
-    const {success} = await ratelimit.limit("my-rate-limit"); //can use any identifier here, like user ID, so that the limit depends on each user
+    const {success} = await ratelimit.limit(`limit:${userId}`); //can use any identifier here, like user ID, so that the limit depends on each user
     if (!success) {
       return res.status(429).json({ 
         message: "Too many requests, please try again later." 
