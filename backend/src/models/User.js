8d77bf3f98eca6c740b70de53cd222c2c6968e9c
Cloudyday56 <Rchen78900@gmail.com>
2025-07-24 12:47:21 -0400

    Auth system - New User model and Note model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -4,24 +4,29 @@ import mongoose from "mongoose";
 // 1. create schema
 // 2. create model based on the schema
 
-const noteSchema = new mongoose.Schema({
-  title: {
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fullName: {
     type: String,
     required: true,
   },
-  content: {
+  password: {
     type: String,
     required: true,
+    minlength: 6,
   },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User", // Reference to the User model
-    required: true,
+  profilePic: {
+    type: String,
+    default: "",
   },
 }, {
   timestamps: true, // Automatically adds createdAt and updatedAt fields
 });
 
-const Note = mongoose.model("Note", noteSchema);
+const User = mongoose.model("User", userSchema);
 
-export default Note;
+export default User;
