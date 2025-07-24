528b09e7171176728a51d3a623eccc76bb600965
Cloudyday56 <Rchen78900@gmail.com>
2025-07-24 18:00:35 -0400

    Auth system - User data undefined issue solved

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,10 +1,13 @@
 // get authentication functions from backend
 
-import { create } from 'zustand'; //provides functions like set()
-import axios from 'axios';
-import toast from 'react-hot-toast';
+import { create } from "zustand"; //provides functions like set()
+import axios from "axios";
+import toast from "react-hot-toast";
 
-const API_URL = import.meta.env.MODE === "development" ? 'http://localhost:5001/api/auth' : '/api/auth';
+const API_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api/auth"
+    : "/api/auth";
 
 axios.defaults.withCredentials = true; //to allow cookies to be sent with requests
 
@@ -18,11 +21,19 @@ export const useAuthStore = create((set) => ({
   message: null,
 
   //signup
-  signup: async (email, password, name) => {
+  signup: async (email, password, fullName) => {
     set({ isLoading: true, error: null }); //start loading while signing up
     try {
-      const response = await axios.post(`${API_URL}/signup`, { email, password, name });
-      set({ user: response.data, isAuthenticated: true, isLoading: false });
+      const response = await axios.post(`${API_URL}/signup`, {
+        email,
+        password,
+        fullName,
+      });
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isLoading: false,
+      });
     } catch (error) {
       set({ error: error.response.data.message, isLoading: false });
       throw error;
@@ -34,18 +45,31 @@ export const useAuthStore = create((set) => ({
     set({ isCheckingAuth: true, error: null }); //start checking auth
     try {
       const response = await axios.get(`${API_URL}/check-auth`);
-      set({ user: response.data, isAuthenticated: true, isCheckingAuth: false });
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isCheckingAuth: false,
+      });
     } catch (error) {
       set({ error: null, isCheckingAuth: false, isAuthenticated: false });
     }
   },
 
   //login
-  login: async( email, password ) => {
+  login: async (email, password) => {
     set({ isLoading: true, error: null }); //start loading while logging in
     try {
-      const response = await axios.post(`${API_URL}/login`, { email, password });
-      set({ user: response.data, isAuthenticated: true, isLoading: false, error: null });
+      const response = await axios.post(`${API_URL}/login`, {
+        email,
+        password,
+      });
+      console.log("Login response:", response.data.user);
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isLoading: false,
+        error: null,
+      });
     } catch (error) {
       set({ error: error.response.data.message, isLoading: false });
       throw error;
@@ -57,38 +81,46 @@ export const useAuthStore = create((set) => ({
     set({ isLoading: true, error: null }); //start loading while logging out
     try {
       await axios.post(`${API_URL}/logout`);
-      toast.success('Logged out successfully');
-      set({ user: null, isAuthenticated: false, isLoading: false, error: null });
+      toast.success("Logged out successfully");
+      set({
+        user: null,
+        isAuthenticated: false,
+        isLoading: false,
+        error: null,
+      });
     } catch (error) {
       set({ error: error.response.data.message, isLoading: false });
       throw error;
     }
   },
 
-  updateProfile: async(data) => {
-    set({isUpdatingProfile: true}); //set loading state
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true }); //set loading state
     try {
       const response = await axios.put(`${API_URL}/update-profile`, data); //send the profile update request
-      set({user: response.data, isAuthenticated: true, isUpdatingProfile: false, error: null}); //update user state with new profile data
-      toast.success('Profile updated successfully!');
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isUpdatingProfile: false,
+        error: null,
+      }); //update user state with new profile data
+      toast.success("Profile updated successfully!");
     } catch (error) {
-      console.log('Error updating profile:', error);
+      console.log("Error updating profile:", error);
       toast.error(error.response.data.message);
     } finally {
-      set({isUpdatingProfile: false}); //reset loading state
+      set({ isUpdatingProfile: false }); //reset loading state
     }
   },
 
   // delete user account
   deleteAccount: async () => {
-  try {
-    await axios.delete(`${API_URL}/delete-account`); //send request to backend endpoint
-    set({user: null, isAuthenticated: false, error: null}); // Reset auth user state
-    toast.success('Account deleted successfully');
+    try {
+      await axios.delete(`${API_URL}/delete-account`); //send request to backend endpoint
+      set({ user: null, isAuthenticated: false, error: null }); // Reset auth user state
+      toast.success("Account deleted successfully");
     } catch (error) {
-      toast.error('Error deleting account. Please try again.');
+      toast.error("Error deleting account. Please try again.");
     }
   },
-
-}))
-
+}));
