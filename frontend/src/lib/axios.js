b4bf9301d81d5d65702583e43a936059ca16cbe0
Cloudyday56 <Rchen78900@gmail.com>
2025-07-25 18:24:55 -0400

    Deployment test - again

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const BASE_URL = import.meta.env.MODE === "development" ? "/api" : "https://backend-6z9h.onrender.com/api";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: BASE_URL,
 });
 
 export default api;
