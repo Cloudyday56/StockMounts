941fd6417243e7bb9715ba8023e3ebc06fb8d08d
Cloudyday56 <Rchen78900@gmail.com>
2025-07-25 16:01:22 -0400

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const BASE_URL = import.meta.env.MODE === "development" ? "/api" : "https://backend-6z9h.onrender.com/api";
 
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "/api",
 });
 
 export default api;
