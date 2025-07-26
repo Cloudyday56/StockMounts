80a6f352dbe99aaf75e4a4d7b960db447548f9b5
Cloudyday56 <Rchen78900@gmail.com>
2025-07-26 19:33:56 -0400

    try Caddy?

--- backend/src/utils/generateTokenAndSetCookie.js
+++ backend/src/utils/generateTokenAndSetCookie.js
@@ -1,19 +1,18 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export const generateTokenAndSetCookie = (res, userId) => {
   // Generate a JWT token
   const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: '7d' // Token valid for 7 days
+    expiresIn: "7d", // Token valid for 7 days
   });
 
   // Set the token in a cookie
-  res.cookie('token', token, {
+  res.cookie("token", token, {
     httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
-    secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
-    sameSite: 'Strict', // Helps prevent CSRF attacks
-    maxAge: 7 * 24 * 60 * 60 * 1000 // Cookie expires in 7 days
+    secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Strict", // 'None' for cross-site in prod, 'Strict' for local dev
+    maxAge: 7 * 24 * 60 * 60 * 1000, // Cookie expires in 7 days
   });
 
   return token;
-}
-
+};
