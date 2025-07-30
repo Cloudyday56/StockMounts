7185e92cb658534972712881990bb229cb0c0c26
Cloudyday56 <Rchen78900@gmail.com>
2025-07-30 18:02:00 -0400

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-// Change "/api" to "http://localhost:5001/api" for non-docker development
 const BASE_URL = import.meta.env.MODE === "development" ? "/api" : "https://backend-6z9h.onrender.com/api";
+// Change "/api" to "http://localhost:5001/api" for non-docker development
 
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: BASE_URL, // use "/api" for local Docker production
 });
 
 export default api;
